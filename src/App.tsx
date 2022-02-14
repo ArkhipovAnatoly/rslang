5d64193739edeed11fcfe277f514db5aa72d72b3
DaryaEnina <78152480+DaryaEnina@users.noreply.github.com>
2022-02-14 14:09:38 +0300

    fix: branch conflict

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import './Components/LogIn/LogIn.css';
 import Home from './Components/Home/Home';
-// import Book from './Components/Dictionary/Book';
+import Book from './Components/Dictionary/Book';
 import { GlobalContextProvider } from './GlobalContext';
 import Authorization from './Components/LogIn/Authorization/Authorization';
 import Registration from './Components/LogIn/Registration/Registration';
@@ -15,7 +15,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/registration" element={<Registration />} />
                 <Route path="/authorization" element={<Authorization />} />
-                {/* <Route path="/book/:group/:page" element={<Book />} /> */}
+                <Route path="/book/:group/:page" element={<Book />} />
             </Routes>
         </GlobalContextProvider>
     );
