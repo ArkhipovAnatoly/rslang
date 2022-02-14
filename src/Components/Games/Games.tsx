d5ab7388abf681d82e7dbd629a8b6597cec8947b
Marina2609 <78434449+Marina2609@users.noreply.github.com>
2022-02-14 12:04:02 +0300

    feat: add game pages

---
+++ src/Components/Games/Games.tsx
@@ -0,0 +1,49 @@
+import './Games.css'
+
+const Games = () => (
+  <div className="games-page">
+    <nav>
+      <div id="top" className="nav-wrapper blue">
+        <ul id="nav-mobile" className="right">
+            <li>
+                <a href="/">Главная</a>
+            </li>
+            <li>
+                <a href="/book/1/1">Книга</a>
+            </li>
+        </ul>
+      </div>
+    </nav>
+    <div className="cards">
+      <div className="card">
+        <div className="card-image waves-effect waves-block waves-light">
+          <img className="image" src={require("./images/2.png")} alt="audio game"/>
+        </div>
+        <div className="card-content">
+          <span className="card-title activator grey-text text-darken-4">Аудиовызов<i className="material-icons right">more_vert</i></span>
+          <p><a href="/audioGame/level/1">Начать игру</a></p>
+        </div>
+        <div className="card-reveal">
+          <span className="card-title grey-text text-darken-4">Аудиовызов<i className="material-icons right">close</i></span>
+          <p>Тренировка Аудиовызов улучшает твое восприятие речи на слух.</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-image waves-effect waves-block waves-light">
+          <img className="image" src={require("./images/1.png")} alt="sprint"/>
+        </div>
+        <div className="card-content">
+          <span className="card-title activator grey-text text-darken-4">Спринт<i className="material-icons right">more_vert</i></span>
+          <p><a href="/sprint/level/1">Начать игру</a></p>
+        </div>
+        <div className="card-reveal">
+          <span className="card-title grey-text text-darken-4">Спринт<i className="material-icons right">close</i></span>
+          <p>Спринт - тренировка на скорость. 
+            Попробуй угадать как можно больше слов за 30 секунд.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Games;
---
+++ src/Components/Games/Sprint/Sprint.tsx
@@ -0,0 +1,64 @@
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+
+const Sprint = () => {
+  const navigator = useNavigate();
+  const [show, setShow] = useState(false); 
+
+  const gameLevel = () => {
+    if(!show) {
+      setShow(!show)
+    }
+  }
+
+  const handlerGroup = (event: React.MouseEvent) => {
+    const { dataset } = event.target as HTMLDivElement;
+  
+    if (!dataset.group){
+      return;
+    }
+  
+    const currentGroup = dataset.group;
+   
+    navigator(`/sprint/level/${currentGroup}`);
+    gameLevel();
+  };
+
+  return (
+    <div className="games-page">
+      <nav>
+        <div id="top" className="nav-wrapper blue">
+          <ul id="nav-mobile" className="right">
+              <li>
+                  <a href="/">Главная</a>
+              </li>
+              <li>
+                  <a href="/book/1/1">Книга</a>
+              </li>
+              <li>
+                  <a href="/games" className="btn-main">Игры</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="card">
+          <div className="card-content">
+            <div className="content">
+            <h1>Игра Спринт</h1>
+            <h2>Выберите уровень сложности</h2>
+            <div className="groups" aria-hidden  onClick={handlerGroup} >
+              <span className="level" data-group="1">A1</span>
+              <span className="level" data-group="2">A2</span>
+              <span className="level" data-group="3">B1</span>
+              <span className="level" data-group="4">B2</span>
+              <span className="level" data-group="5">C1</span>
+              <span className="level" data-group="6">C2</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sprint;
