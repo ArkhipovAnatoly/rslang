3ddc728ca25207b6c246a1e871a409e072cdefeb
Anatoly Arkhipov <atol83@mail.ru>
2022-02-15 20:46:04 +0300

    refactor: apply prettier

--- src/Components/Games/Games.tsx
+++ src/Components/Games/Games.tsx
@@ -19,7 +19,12 @@ const Games = () => {
                             Аудиовызов<i className="material-icons right">more_vert</i>
                         </span>
                         <p>
-                            <Link to={group && page ? `/audioGame/${group}/${page}` : '/audioGame'} className="open-game"><span data-hover="Начать игру">Начать игру</span></Link>
+                            <Link
+                                to={group && page ? `/audioGame/${group}/${page}` : '/audioGame'}
+                                className="open-game"
+                            >
+                                <span data-hover="Начать игру">Начать игру</span>
+                            </Link>
                         </p>
                     </div>
                     <div className="card-reveal">
@@ -38,7 +43,9 @@ const Games = () => {
                             Спринт<i className="material-icons right">more_vert</i>
                         </span>
                         <p>
-                            <Link to={group && page ? `/sprint/${group}/${page}` : '/sprint'} className="open-game"><span data-hover="Начать игру">Начать игру</span></Link>
+                            <Link to={group && page ? `/sprint/${group}/${page}` : '/sprint'} className="open-game">
+                                <span data-hover="Начать игру">Начать игру</span>
+                            </Link>
                         </p>
                     </div>
                     <div className="card-reveal">
