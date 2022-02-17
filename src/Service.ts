2bdeb4531757e0660ba98b9f859587d455289f84
Anatoly Arkhipov <atol83@mail.ru>
2022-02-17 14:41:47 +0300

    feat: add field

--- src/Service.ts
+++ src/Service.ts
@@ -69,6 +69,7 @@ export type DataAggregatedWordsById = {
         difficulty: string;
         optional: {
             guessedCount: string;
+            notGuessedCount: string;
         };
     };
 };
