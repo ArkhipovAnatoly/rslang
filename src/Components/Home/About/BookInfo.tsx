c9f84464a425e2d2896a9deebfc4eae913382d5d
Gothica <semidevilish666@gmail.com>
2022-02-18 19:27:12 +0300

    feat: pages About

--- src/Components/Home/About/BookInfo.tsx
+++ src/Components/Home/About/BookInfo.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './about.css';
 import Header from '../Header';
 import Footer from '../Footer';
 import Menu from '../../Menu/Menu';
+
 
 const BookInfo = () => {
 const [menuActive, setMenuActive] = useState<boolean>(false);
@@ -11,7 +13,17 @@ const [menuActive, setMenuActive] = useState<boolean>(false);
     <div className="info_wrapper">
         <Header menuActive={menuActive} setMenuActive={setMenuActive} />
         <Menu menuActive={menuActive} setMenuActive={setMenuActive} />
-        BookInfo
+        
+        <h2 className='info-title'>Словарь</h2>
+        <div className='info-image1'/>
+        <div className='info-description'>
+            <p>В учебнике собраны 3600 самых используемых в повседневной жизни слов, есть его определение и примеры использования как на русском, так и на английском!</p>
+            <p>Слова в коллекции отсортированы от более простых и известных к более сложным.</p>
+            <p>Вся коллекция разбита на шесть разделов, в каждом разделе 30 страниц, на каждой странице 20 слов для изучения. Cедьмой раздел учебника - Сложные слова</p>
+            <p>Все слова, которые ты изучил попадают в твой личный словарь. Можно отметить сложные для тебя слова, чтобы знать, на что чаще обращать внимание! </p>
+            <p>Если сложное слово стало изученным, оно перестаёт быть сложными и удаляется из раздела Сложные слова</p>
+        </div>
+        <Link className='arrow-back' to="/"/>
         <Footer />
     </div>
 )};
--- src/Components/Home/About/GameInfo.tsx
+++ src/Components/Home/About/GameInfo.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './about.css';
 import Header from '../Header';
 import Footer from '../Footer';
@@ -11,7 +12,18 @@ const GameInfo = () =>{
         <div className="info_wrapper">
             <Header menuActive={menuActive} setMenuActive={setMenuActive} />
             <Menu menuActive={menuActive} setMenuActive={setMenuActive} />
-            BookInfo
+            <h2 className='info-title'>Игры</h2>
+            <div className='info-image2'/>
+            <div className='info-description'>
+                <p>Игры сделают изучение слов более увлекательным и продуктивным:</p>
+                <p>- игровой процесс влияет на улучшение запоминания</p>
+                <p>- развивают восприятие на слух и навыки перевода</p>
+                <p>- можно выбрать один из шести уровней сложности</p>
+                <p>Если размещённых на странице слов для игры недостаточно, задействуются слова с предыдущих страниц, закрепляя знания. </p>
+                <p>Игра “Аудиовызов” поможет тебе разить навыки аудирования и перевода.</p>
+                <p>В Игре “Спринт” поможет развить навыки перевода. </p>
+            </div>
+            <Link className='arrow-back' to="/"/>
             <Footer />
         </div>
     )};
