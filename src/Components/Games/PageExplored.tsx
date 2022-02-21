0e604a056ebd31383afa05254bb72ba042f0e386
Marina2609 <78434449+Marina2609@users.noreply.github.com>
2022-02-21 19:05:13 +0300

    feat: add link to book page

--- src/Components/Games/PageExplored.tsx
+++ src/Components/Games/PageExplored.tsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import Footer from "../Home/Footer";
 import Header from "../Home/Header";
 import Menu from "../Menu/Menu";
 import './PageExplored.css'
 
+type DataParams = {
+  group: string;
+  page: string;
+};
+
 const PageExplored = () => {
   const [menuActive, setMenuActive] = useState<boolean>(false);
-
+  const { group, page } = useParams<DataParams>();
+  
   return(
     <div className="explored">
       <Header menuActive={menuActive} setMenuActive={setMenuActive} />
       <Menu menuActive={menuActive} setMenuActive={setMenuActive} />
       <div className="page-message">
-      <div className="text-explored">
-        <span className="letter">ТРЕНИРОВКИ НЕ НУЖНЫ.</span>
-        <span className="letter">НА ЭТОЙ СТРАНИЦЕ</span>
-        <span className="letter">ВСЕ ИЗУЧЕНО!</span>
+        <div className="text-explored">
+          <span className="letter">ТРЕНИРОВКИ НЕ НУЖНЫ.</span>
+          <span className="letter">НА ЭТОЙ СТРАНИЦЕ</span>
+          <span className="letter">ВСЕ ИЗУЧЕНО!</span>
+          <p>
+            <Link to={`/book/${group}/${page}`} className="return-book">
+                <span data-hover="Вернуться на главную страницу">Вернуться на главную страницу</span>
+            </Link>
+          </p>
         </div>
       </div>
       <Footer />
